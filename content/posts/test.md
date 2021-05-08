0fd303e8edafa038c3a9fc76626349507ce5fa99
last night <fujizhang2525@gmail.com>
2021-05-08 12:05:03 -0400

    update

--- content/posts/test.md
+++ content/posts/test.md
@@ -1,8 +1,8 @@
 ---
 date: 2021-05-07T23:19:05Z
-hero_image: "/content/images/sonja-punz-N47B_zibNGo-unsplash.jpg"
 title: test
 author: 'Simon '
+hero_image: "../content/images/sonja-punz-N47B_zibNGo-unsplash.jpg"
 
 ---
 test 
--- gatsby-node.js
+++ gatsby-node.js
@@ -45,3 +45,16 @@ module.exports.createPages = async ({ graphql, actions }) => {
     })
   })
 }
+
+exports.createSchemaCustomization = ({ actions }) => {
+  const { createTypes } = actions
+
+  createTypes(`
+    type Mdx implements Node {
+      frontmatter: MdxFrontmatter!
+    }
+    type MdxFrontmatter {
+      hero_image: File @fileByRelativePath
+    }
+  `)
+}
