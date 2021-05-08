f8996d329996f6cf8ee946e844524cf21db9fc9e
last night <fujizhang2525@gmail.com>
2021-05-08 14:52:17 -0400

    init design (to be fixed)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,26 @@
 import React from "react"
 import Layout from "../components/Layout"
 //import BlogList from "../components/BlogList"
-
+import indexStyles from "../styles/pages/index.module.scss"
 export default function IndexPage() {
   return (
-    <Layout page="home" bgColor="inherit">
-      <section>
-        <div>
-          <h3>test</h3>
-        </div>
-      </section>
-    </Layout>
+    <section className={indexStyles.layout} >
+      <article className={indexStyles.layoutItem}>
+          <div>
+          <h1>art from last night</h1>
+          <a href="https://https://www.behance.net/artlastnight">
+            <p>portfolio</p>
+          </a>
+          <a href="https://www.instagram.com/art.last.night/">
+            <p>instagram</p>
+          </a>
+          </div>
+         <div>
+           <p>Contact Me</p>
+           <a href="mailto:work@lastnight.online">work@lastnight.online</a>
+
+         </div>
+      </article>
+    </section>
   )
 }
